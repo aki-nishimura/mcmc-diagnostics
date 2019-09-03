2860762589355b7a3b9c3ee347ad2b100d2f70fe
Aki Nishimura <akihiko4@g.ucla.edu>
2019-09-03 16:34:16 -0700

    setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mc_coda',
+    version='0.1',
+    description='Python analogue of the R coda package',
+    url='https://github.com/aki-nishimura/mcmc-diagnostic',
+    author='Akihiko (Aki) Nishimura',
+    author_email='akihiko4@ucla.edu',
+    license='MIT',
+    packages=find_packages()
+)
